755ad2fc7f76135ee1dbd9c56437e2768434cd94
DidierOne <dpd5518@psu.edu>
2020-09-03 21:59:18 +0000

    excludes stdlib

--- main.c
+++ main.c
@@ -1,15 +1,14 @@
 // author Daniel Dean dpd5518@psu.edu
 // contributor Seulki Kim svk6271@psu.edu
 // contributor Gloria Kang yjk5152@psu.edu
-// // contributor Heonyeong Ha hzh5274@psu.edu
+// contributor Heonyeong Ha hzh5274@psu.edu
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <readline/readline.h>
-#include <stdlib.h>
 
 int main(void) {
-  char * temp = readline("Enter temperature in celsius: ");
+  char *temp = readline("Enter temperature in celsius: ");
 
   double tempC = atof(temp);
   double tempF = ( tempC * 9/5) + 32;
